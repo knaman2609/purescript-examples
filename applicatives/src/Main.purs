7648539835d7b571d111db4c41e8d86736806b79
Naman <naman.kalkhuria@juspay.in>
2020-08-30 22:07:01 +0530

    error array implemented

--- applicatives/src/Main.purs
+++ applicatives/src/Main.purs
@@ -5,22 +5,39 @@ import Effect (Effect)
 import Effect.Console (log)
 import Data.Either
 import Data.Maybe
+import Data.Array
 
+newtype Validation  r  = Validation (Either (Array String) r)
+derive instance validationFunctor :: Functor Validation
+
+-- instance validationShow :: Show (Validation m) where
+--   show (Validation x) = show x
+
+instance applyValidation :: Apply (Validation) where
+  apply (Validation (Left x)) (Validation (Left y)) =  Validation (Left $  (x <>  y))
+  apply (Validation (Left x)) (Validation (Right y)) =  Validation (Left x)
+  apply (Validation (Right f)) r =  f <$> r
+
+
+
+withError :: forall a. Maybe a  -> Array String -> Validation  a
+withError Nothing  err = Validation (Left err)
+withError (Just a) _   = Validation (Right a)
+
+fullName :: String -> String -> String -> String
 fullName first middle last = last <> ", " <> first <> " " <> middle
+
+fullNameEither :: Maybe String -> Maybe String -> Maybe String -> Validation String
+fullNameEither first middle last =
+  map fullName (withError first ["fname missing"])
+  <*> (withError middle  ["Middle name was missing"])
+  <*> ( withError last ["Last name was missing"])
 
 main :: Effect Unit
 main = do
-  let x = fullName <$> Just "Phillip" <*> Just "A" <*> Just "c"
-  log $ show x
+  let x = fullNameEither  (Nothing) (Just "Naman") (Nothing)
 
-
-  -- | ``` purescript
-  -- | f <$> Nothing <*> Just y == Nothing
-  -- | f <$> Just x <*> Nothing == Nothing
-  -- | f <$> Nothing <*> Nothing == Nothing
-  -- | ```
--- instance applyMaybe :: Apply Maybe where
---   apply (Just fn) x = fn <$> x
---   apply Nothing   _ = Nothing
-
--- above code evals from left to right
+  -- log $ show x
+  case x of
+    Validation (Right r) -> log $ show r
+    Validation (Left e) -> log $ show e
