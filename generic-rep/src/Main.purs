b0027981a3f706f2107ac91ee4fc472961d60177
Naman <naman.kalkhuria@juspay.in>
2020-09-01 22:34:26 +0530

    generics-rep working

--- generic-rep/src/Main.purs
+++ generic-rep/src/Main.purs
@@ -1,15 +1,16 @@
 module Main where
 
+import Prelude
+import Effect (Effect)
+import Effect.Class.Console
+
 import Data.Generic.Rep
 import Data.Generic.Rep.Show
-import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
 data Foo  = Foo String
 derive instance genericFoo :: Generic (Foo) _
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Effect  Unit
 main = do
   log $ genericShow (Foo "hello")
